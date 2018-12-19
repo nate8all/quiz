d750ebee3d40a37f680d78c4e5cdfc64944516c3
Nathan Alleyne <nathan.alleyne7@gmail.com>
2018-12-19 22:54:01 +0000

    Second day, functions

--- js/script.js
+++ js/script.js
@@ -8,7 +8,23 @@ $(document).ready(function() {
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
-        var totalScore;
+        var q4Result = $("#question4").val();
+        var totalScore = 0;
     });
 
 });
+
+function quest1(answer){
+    if (answer === "pizza") {
+        return 3;
+    }
+    else if (answer === "chicken"){
+        return 6;
+    }
+    else if (answer === "fish") {
+        return 9;
+    }
+    else {
+        return 12;
+    }
+}
