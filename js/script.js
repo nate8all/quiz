c8313455916f062c1c0d48b718997db63d1a4a79
Nathan Alleyne <nathan.alleyne7@gmail.com>
2019-02-13 21:17:54 +0000

    More partial editing

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         var q3Result = $("#question3").val();
         var q4Result = $("#question4").val();
         var q5Result = $("#question5").val();
-        
+        var totalScore = 0;
         
         name.toLowerCase();
         q2Result.toLowerCase();
@@ -17,25 +17,12 @@ $(document).ready(function() {
         q4Result.toLowerCase();
         q5Result.toLowerCase();
         
-        //var score1 = quest1(name);
-        //var score2 = quest2(q2Result);
-        //var score3 = quest3(q3Result);
-        //var score4 = quest4(q4Result);
-        //var score5 = quest5(q5Result);
-        var totalScore = 0;
-        
-        //totalScore += score1;
-        //totalScore += score2;
-        //totalScore += score3;
-        //totalScore += score4;
-        //totalScore += score5;
         totalScore += quest1(name);
         totalScore += quest2(q2Result);
         totalScore += quest3(q3Result);
         totalScore += quest4(q4Result);
         totalScore += quest5(q5Result);
-        //score1 + score2 + score3 + score4 + score5;
-
+        
         total(totalScore,spiritAnimal);
     });
 });
@@ -128,39 +115,30 @@ function quest5(answer){
     else {
         //score5 = 12;
         return 12;
-    }
+    }}
     
-}
-
-
 function total(score, animal) {
     if (score <= 15) {
         animal = "Duck";
         $(".result").html("<img id='resultphoto' src=\"https://cdn.vox-cdn.com/thumbor/Wd-2zsFlWxXz1HgmGEEwXx5iYBI=/0x0:1000x605/1200x800/filters:focal(420x223:580x383)/cdn.vox-cdn.com/uploads/chorus_image/image/48780381/duck.0.png\">");
-        //$("img").attr("src", "https://cdn.vox-cdn.com/thumbor/Wd-2zsFlWxXz1HgmGEEwXx5iYBI=/0x0:1000x605/1200x800/filters:focal(420x223:580x383)/cdn.vox-cdn.com/uploads/chorus_image/image/48780381/duck.0.png");
         $(".result").append("<h1>Your Spirit Animal is the " + animal + "!</h1>");
         $(".result").append("<h3>Don't be too bummed. The duck is often seen to symbolize freedom, as well as high values and admirable since they posses many skills, like swimming and flying.</h3>");
     }
     else if (score > 15 && score <= 30) {
         animal = "Dolphin";
         $(".result").html("<img id='resultphoto' src=\"https://www.dolphindiscovery.com/imagesNuevo/imgsLocs/anguilla/anguilla-9.jpg?v=7.8.16\">");
-        //$("img").attr("src", "https://www.dolphindiscovery.com/imagesNuevo/imgsLocs/anguilla/anguilla-9.jpg?v=7.8.16");
         $(".result").append("<h1>Your Spirit Animal is the " + animal + "!</h1>");
         $(".result").append("<h3>Dolphins are known to be gentle, calming, smooth and easy-going. That must be the type of person you are!</h3>");
     }
     else if (score > 30 && score <= 45) {
         animal = "Elephant";
         $(".result").html("<img id='resultphoto' src=\"https://www.futurity.org/wp/wp-content/uploads/2018/08/elephant-close-up_1600.jpg\">");
-        //$("img").attr("src", "ttps://www.futurity.org/wp/wp-content/uploads/2018/08/elephant-close-up_1600.jpg");
         $(".result").append("<h1>Your Spirit Animal is the " + animal + "!</h1>");
         $(".result").append("<h3>According to other online sources, the elephant usually symbolizes strength, stability, honor, and perserverance. Go you!</h3>");
     }
     else if (score > 45 && score <= 60) {
         animal = "Lion, King of the Jungle";
         $(".result").html("<img id='resultphoto' src=\"https://www.gannett-cdn.com/presto/2018/10/19/PIND/17898f77-71af-484e-b39f-df4c132c47fd-Nyack.jpg?crop=1395,778,x0,y367&width=3200&height=1680&fit=bounds\">");
-        //$("img").attr("src", "https://www.gannett-cdn.com/presto/2018/10/19/PIND/17898f77-71af-484e-b39f-df4c132c47fd-Nyack.jpg?crop=1395,778,x0,y367&width=3200&height=1680&fit=bounds");
         $(".result").append("<h1>Your Spirit Animal is the " + animal + "!</h1>");
         $(".result").append("<h3>In terms of being a spirit animal, a lion can represent strength, assertiveness, might, courage, justice, and many ohers. et out that lion!</h3>");
-    }
-    
-}
+    }}
